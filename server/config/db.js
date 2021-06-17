f7094135e33685d9a7f63937214f78b8dfaf4b80
balvara1 <balvara1@my.centennialcollege.ca>
2021-06-17 02:59:16 -0400

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/contact_list"
+    //"URI": "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://bernadette:lHLNl9Gt2lAkq8JZ@mongodbserver.dgbsp.mongodb.net/contact_list?retryWrites=true&w=majority"
 }
